ce86c8222bbae05d2e75e3cbb982df2d8bd7597c
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-10-21 13:47:00 +0300

    initialised the app

--- main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.bars li .bar').each(function(key, bar){
-        let percentage = $(this).data('percentage');
-        $(this).animate({
-            'height': percentage + '%'
-        }, 2000);
-    });
-});
