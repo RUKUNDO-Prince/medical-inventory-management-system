66c18adb264213047c1099411182661e9b92496c
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-10-22 14:50:52 +0300

    added the signup form

--- medical_inventory/views.py
+++ medical_inventory/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
-from medical_inventory_management_system.medical_inventory.forms import UserRegisterForm
+from .forms import UserRegisterForm
+
 
 class Index(TemplateView):
     template_name = 'medical_inventory/index.html'
