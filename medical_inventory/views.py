b95880b52b59a726d7d936aed9b949e635c152df
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-10-25 10:06:42 +0300

    successfully adding new medical items

--- medical_inventory/views.py
+++ medical_inventory/views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, View
-from .forms import UserRegisterForm
-from .models import InventoryItem
+from django.views.generic import TemplateView, View, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .forms import UserRegisterForm, InventoryItemForm
+from .models import InventoryItem, Category
 
 class Index(TemplateView):
     template_name = 'medical_inventory/index.html'
 
-class Dashboard(View):
+class Dashboard(LoginRequiredMixin, View):
     def get(self, request):
         items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
         return render(request, 'medical_inventory/dashboard.html', {'items': items})
@@ -30,3 +32,18 @@ class SignUpView(View):
             login(request, user)
             return redirect('index')
         return render(request, 'medical_inventory/signup.html', {'form': form})
+
+class AddItem(LoginRequiredMixin, CreateView):
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = 'medical_inventory/item_form.html'
+    success_url = reverse_lazy('dashboard')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
